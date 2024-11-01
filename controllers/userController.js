9aacd143608ef2744678b4b3eaad61cedcbcdfa8
ST3WIN <stewinkurissingal@gmail.com>
2024-11-01 14:26:59 +0530

    dataStored

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,29 @@ const loadRegister = async(req,res)=>{
         console.log(error.message)
     }
 }
+
+const insertUser = async(req,res)=>{
+    try {
+        console.log("Request Body:", req.body)
+        const user = new User({
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            is_admin:0
+        });
+        const userData = await user.save();
+        
+        if(userData){
+            res.render("registration",{message:"Succesfully registered!"})
+        }else{
+            res.render("registration",{message:"Registration failed!"})
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
 module.exports ={
-    loadRegister
+    loadRegister,
+    insertUser
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const user_route = express()
-const userController = require("../controllers/userController")
+
 
 user_route.set("view engine","ejs")
 user_route.set("views","./views/users")
 
+const bodyParser = require("body-parser")
+user_route.use(bodyParser.json())
+user_route.use(bodyParser.urlencoded({extended:true}))
+
+
+const userController =  require("../controllers/userController")
+
 user_route.get("/register",userController.loadRegister)
+
+user_route.post("/register",userController.insertUser)
 
 module.exports = user_route
